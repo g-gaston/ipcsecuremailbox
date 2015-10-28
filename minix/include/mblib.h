6920b3d14e6cda979f86d4d510164df9d4554c30
Guillermo Gastón <guillermogastonlorente@gmail.com>
2015-10-28 14:20:51 -0500

    Moved struct definitions

--- minix/include/mblib.h
+++ minix/include/mblib.h
@@ -3,34 +3,6 @@
 
 #include <lib.h>
 #include <unistd.h>
-#include <stdio.h>
-
-typedef struct {
-	char *text;
-	int *receivers_pid;
-	int num_rec;
-	mb_message_t *next;
-} mb_message_t;
-
-typedef struct {
-	int pid;
-	int signum;
-	mb_req_t *next;
-} mb_req_t;
-
-typedef struct {
-	char* name;
-	mb_message_t *first_msg;
-	int num_msg;
-	mb_req_t *first_req;
-	int num_req;
-	mb_mailbox_t *next;
-} mb_mailbox_t;
-
-typedef struct {
-	mb_mailbox_t *first_mb;
-	int num_mbs;
-} mb_mbs_t;
 
 int mb_open(char *name) {
 	message m;
--- minix/servers/pm/mailbox.h
+++ minix/servers/pm/mailbox.h
@@ -2,3 +2,32 @@
 
 
 /* Defines */
+
+
+
+typedef struct {
+	char *text;
+	int *receivers_pid;
+	int num_rec;
+	mb_message_t *next;
+} mb_message_t;
+
+typedef struct {
+	int pid;
+	int signum;
+	mb_req_t *next;
+} mb_req_t;
+
+typedef struct {
+	char* name;
+	mb_message_t *first_msg;
+	int num_msg;
+	mb_req_t *first_req;
+	int num_req;
+	mb_mailbox_t *next;
+} mb_mailbox_t;
+
+typedef struct {
+	mb_mailbox_t *first_mb;
+	int num_mbs;
+} mb_mbs_t;
